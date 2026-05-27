ec6132359e5e710b8f4721fe82876575af8d5676
lllnightlll <playpr991@gmail.com>
2026-05-28 03:52:33 +0600

    update main method for darknet

--- src/main/java/org/example/DarkNet/DarkNet.java
+++ src/main/java/org/example/DarkNet/DarkNet.java
@@ -1,188 +1,44 @@
 package org.example.DarkNet;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.*;
 
+import org.example.DarkNet.Data.Coordinates;
 import org.example.DarkNet.Data.Edge;
-import org.example.DarkNet.Data.Euclid;
 import org.example.DarkNet.Data.Node;
-import org.example.DarkNet.Data.ReadFiles;
+import org.example.DarkNet.Data.Map;
 import org.example.DarkNet.DrawLibs.RayLib;
-
-//import com.raylib.Colors;
-//import com.raylib.jextract.raylib_h;
-
-//import static com.raylib.Colors.*;
-import static com.raylib.Raylib.*;
-import static com.raylib.Helpers.*;
+import org.example.DarkNet.DrawLibs.LibJDX;
 
 public class DarkNet {
     public static final int CANVAS_WIDTH = 1200;
     public static final int CANVAS_HEIGHT = 800;
-    
+
     private static final Scanner in = new Scanner(System.in);
     private static List<Node> nodes;
     private static List<Edge> edges;
     private static HashMap<Long, Integer> nodeIdToPos = new HashMap<>();
 
     private static void initData() {
-        try {
-            nodes = ReadFiles.readNodes("nodes.csv");
-            edges = ReadFiles.readEdges("edges.csv");
-
-            for (int i = 0; i < nodes.size(); i++) {
-                nodeIdToPos.put(nodes.get(i).getId(), i);
-            }
-
-            System.out.println(nodes.size());
-            System.out.println(edges.size());
-
-            double minLon = nodes.get(0).getLon();
-            double maxLon = nodes.get(0).getLon();
-            double minLat = nodes.get(0).getLat();
-            double maxLat = nodes.get(0).getLat();
-
-            for (Node node : nodes) {
-                if (minLat > node.getLat())
-                    minLat = node.getLat();
-                if (minLon > node.getLon())
-                    minLon = node.getLon();
-                if (maxLat < node.getLat())
-                    maxLat = node.getLat();
-                if (maxLon < node.getLon())
-                    maxLon = node.getLon();
-            }
-
-            double deltaLon = maxLon - minLon;
-            double deltaLat = maxLat - minLat;
-            double scale = (double) CANVAS_HEIGHT / Math.min(deltaLat, deltaLon);
-
-            System.out.println(deltaLon + " " + deltaLat);
-            System.out.println(minLon + " " + minLat + "; " + maxLon + " " + maxLat);
-
-            for (Node node : nodes) {
-                node.x = (node.getLon() - minLon) * scale;
-                node.y = CANVAS_HEIGHT - (node.getLat() - minLat) * scale;
-            }
-
-            for (Edge edge : edges) {
-                Node u = nodes.get(nodeIdToPos.get(edge.getU()));
-                Node v = nodes.get(nodeIdToPos.get(edge.getV()));
-                edge.ux = u.x;
-                edge.uy = u.y;
-                edge.vx = v.x;
-                edge.vy = v.y;
-                edge.dist = (long) Euclid.euclideanDist(edge.ux, edge.uy, edge.vx, edge.vy);
-            }
-        } catch (IOException e) {
-            System.err.println("Ошибка чтения CSV: " + e.getMessage());
-            e.printStackTrace();
-        }
+        Coordinates x = Map.initData(nodes, edges, nodeIdToPos);
+        nodes = x.getNodes();
+        edges = x.getEdges();
+        nodeIdToPos = x.getNodeIdToPos();
     }
 
     public static void main(String[] args) {
         initData();
-        RayLib.draw(nodes, edges, nodeIdToPos);
-        /*try {
-            System.out.println("What is your favourite NarkoShop? Kraken or Mega?");
-            String x = (in.nextLine()).toUpperCase();
-            for (int i = 0; i < 10; i++) {
-                System.out.print(".");
-                TimeUnit.MILLISECONDS.sleep(100);
-            }
-            System.out.println();
-            while (!(x.equals("KRAKEN") || x.equals("MEGA"))) {
-                System.out.println("You wrote a wrong NarkoShop...\nWrite again:");
-                x = (in.nextLine()).toUpperCase();
-                for (int i = 0; i < 10; i++) {
-                    System.out.print(".");
-                    TimeUnit.MILLISECONDS.sleep(100);
-                }
-                System.out.println();
-            }
-            if (x.equals("KRAKEN") || x.equals("MEGA")) {
-                System.out.println("CORRECT!");
-            }
-            InitWindow(CANVAS_WIDTH, CANVAS_HEIGHT, "OMSK");
-            SetTargetFPS(144);
-            System.out.println("Write cords: ");
-
-            while (!WindowShouldClose()) {
-                BeginDrawing();
-                ClearBackground(newColor(30, 30, 30, 255));
-
-                for (Node node : nodes) {
-                    DrawCircle((int) node.x, (int) node.y, 1, newColor(104, 124, 124, 255));
-                }
-
-                for (Edge edge : edges) {
-                    DrawLine(
-                            (int) edge.ux, (int) edge.uy,
-                            (int) edge.vx, (int) edge.vy,
-                            newColor(91, 126, 119, 32));
-                }
-                EndDrawing();
-
-                long start = in.nextLong();
-                long end = in.nextLong();
-                List<List<Node>> zakladka_map = new ArrayList<>();
-                if ((x.toUpperCase()).equals("KRAKEN")) {
-                    zakladka_map = (new Kraken()).dijkstraPath(nodes, edges, nodeIdToPos, start, end);
-                } else if ((x.toUpperCase()).equals("MEGA")) {
-                    zakladka_map = (new MEGA()).dijkstraPath(nodes, edges, nodeIdToPos, start, end);
-                }
-
-                if (!zakladka_map.isEmpty()) {
-                    System.out.println("We are search the nearest way...");
-                    List<Node> zakladka = zakladka_map.get(0);
-                    if (zakladka != null && zakladka.size() > 1) {
-                        int z = 0;
-                        for (int i = 0; i < zakladka.size() - 1; i++) {
-                            Node a = zakladka.get(i);
-                            Node b = zakladka.get(i + 1);
-                            if (z == 0) {
-                                BeginDrawing();
-                            } else if (z == 100 || i == zakladka.size() - 2) {
-                                z = -1;
-                                TimeUnit.MILLISECONDS.sleep(100);
-                                EndDrawing();
-                                System.out.println("PLease wait few second...");
-                            }
-                            DrawLine((int) a.x, (int) a.y, (int) b.x, (int) b.y, newColor(139, 232, 208, 64));
-                            z++;
-                        }
-                    }
-                    System.out.println("The shortest path was found...");
-                    TimeUnit.SECONDS.sleep(1);
-                    zakladka = zakladka_map.get(1);
-                    if (zakladka != null && zakladka.size() > 1) {
-                        for (int i = 0; i < zakladka.size() - 1; i++) {
-                            Node a = zakladka.get(i);
-                            Node b = zakladka.get(i + 1);
-                            BeginDrawing();
-                            DrawLine((int) a.x, (int) a.y, (int) b.x, (int) b.y, newColor(121, 250, 242, 255));
-                            EndDrawing();
-                            if (i == zakladka.size() - 2) {
-                                if ((x.toUpperCase()).equals("MEGA")) {
-                                    i = 0;
-                                } else {
-                                    while (true) {
-                                        TimeUnit.MILLISECONDS.sleep(100);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    System.out.println("We have error...\nTry again:");
-                }
-            }
-            CloseWindow();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }*/
+        System.out.println("Do you like java maboy?");
+        String x = (in.nextLine()).toUpperCase();
+        while (!(x.equals("YES") || x.equals("NO"))) {
+            System.out.println("You wrote a wrong ANSWER...\nWrite again:");
+            x = (in.nextLine()).toUpperCase();
+        }
+        if (x.equals("YES")) {
+            LibJDX.draw(nodes, edges, nodeIdToPos);
+        } else {
+            RayLib.draw(nodes, edges, nodeIdToPos);
+        }
     }
 }
---
+++ src/main/java/org/example/DarkNet/Data/Map.java
@@ -0,0 +1,66 @@
+package org.example.DarkNet.Data;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import org.example.DarkNet.DarkNet;
+
+public class Map {
+    public static Coordinates initData(List<Node> nodes, List<Edge> edges, HashMap<Long, Integer> nodeIdToPos) {
+        try {
+            nodes = ReadFiles.readNodes("nodes.csv");
+            edges = ReadFiles.readEdges("edges.csv");
+
+            for (int i = 0; i < nodes.size(); i++) {
+                nodeIdToPos.put(nodes.get(i).getId(), i);
+            }
+
+            System.out.println(nodes.size());
+            System.out.println(edges.size());
+
+            double minLon = nodes.get(0).getLon();
+            double maxLon = nodes.get(0).getLon();
+            double minLat = nodes.get(0).getLat();
+            double maxLat = nodes.get(0).getLat();
+
+            for (Node node : nodes) {
+                if (minLat > node.getLat())
+                    minLat = node.getLat();
+                if (minLon > node.getLon())
+                    minLon = node.getLon();
+                if (maxLat < node.getLat())
+                    maxLat = node.getLat();
+                if (maxLon < node.getLon())
+                    maxLon = node.getLon();
+            }
+
+            double deltaLon = maxLon - minLon;
+            double deltaLat = maxLat - minLat;
+            double scale = (double) DarkNet.CANVAS_HEIGHT / Math.min(deltaLat, deltaLon);
+
+            System.out.println(deltaLon + " " + deltaLat);
+            System.out.println(minLon + " " + minLat + "; " + maxLon + " " + maxLat);
+
+            for (Node node : nodes) {
+                node.x = (node.getLon() - minLon) * scale;
+                node.y = DarkNet.CANVAS_HEIGHT - (node.getLat() - minLat) * scale;
+            }
+
+            for (Edge edge : edges) {
+                Node u = nodes.get(nodeIdToPos.get(edge.getU()));
+                Node v = nodes.get(nodeIdToPos.get(edge.getV()));
+                edge.ux = u.x;
+                edge.uy = u.y;
+                edge.vx = v.x;
+                edge.vy = v.y;
+                edge.dist = (long) Euclid.euclideanDist(edge.ux, edge.uy, edge.vx, edge.vy);
+            }
+            return new Coordinates(nodes, edges, nodeIdToPos);
+        } catch (IOException e) {
+            System.err.println("Ошибка чтения CSV: " + e.getMessage());
+            e.printStackTrace();
+            return new Coordinates();
+        }
+    }
+}
